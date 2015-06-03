8724c766d8e5e469556ba704f557290fdfed6014
Helen Sherwood-Taylor <helen@rrdlabs.co.uk>
2015-06-03 11:02:36 +0100

    Python3 compatibility fixes

--- shibboleth/views.py
+++ shibboleth/views.py
@@ -7,9 +7,8 @@ from django.core.exceptions import ImproperlyConfigured
 from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.utils.decorators import method_decorator
+from django.utils.six.moves.urllib_parse import quote
 from django.views.generic import TemplateView
-
-from urllib import quote
 
 #Logout settings.
 from shibboleth.app_settings import LOGOUT_URL, LOGOUT_REDIRECT_URL, LOGOUT_SESSION_KEY
